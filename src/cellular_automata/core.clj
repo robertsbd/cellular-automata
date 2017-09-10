80cae22f8e6918380f09e4a84cd9eab83b7d88c6
robertsbd <roberts.b.d@gmail.com>
2017-09-10 19:36:25 +0100

    efficiency improvements

--- src/cellular_automata/core.clj
+++ src/cellular_automata/core.clj
@@ -1,88 +1,72 @@
 (ns cellular-automata.core
   (:gen-class)
-  (:require [clojure.core.matrix :as m])
   (:use [clojure.pprint]))
 
-(def height 60)
-(def width 60)
+(defn lazy-dead-alive-seq [] (repeatedly #(if (= 0 (rand-int 2)) :dead :alive)))
 
 (defn init-matrix [w h]
   "this will create a matrix of height by width of random number"
-  (vec (take h (repeatedly
-                #(vec (take w (repeatedly
-                              (fn [] (rand-int 2)))))))))
+  (take h (repeatedly #(take w (lazy-dead-alive-seq)))))
 
-;; construct 4 lists that contain that required values of north, south, east, and west
+(defn move-rows-south [matrix w h]
+  "moves all the rows south, adding a row of dead cells to the top row"
+  (cons (repeat w :dead) (take (- h 1) matrix)))
 
-(defn empty-top-row [mat]
-  "change all values in the top row to empty"
-  (vec
-   (cons
-   (vec (repeat width 0))
-   (vec (take (- width 1) mat)))))
-
-(defn empty-bot-row [mat]
-  "change all values in the bottom row to empty"
-  (conj
-   (vec (drop 1 mat))
-   (vec (repeat width 0))))
-
-(defn get-north [mat]
-  (empty-top-row mat))
-
-(defn get-south [mat]
-  (empty-bot-row mat))
-
-(defn get-west [mat]
-  (m/transpose (empty-top-row mat)))
-
-(defn get-east [mat]
-  (m/transpose (empty-bot-row mat)))
-
-(defn move-row-east [row]
-  (conj (vec (drop 1 row)) 0))
+(defn move-rows-north [matrix w]
+  "moves all rows north, adding a row of dead cells to the bottom row"
+  (seq
+   (conj (vec (drop 1 matrix)) (repeat w :dead))))
 
 (defn move-row-west [row]
-  (vec (cons 0 (take (- width 1) row))))
+  "moves a single row west, adding a dead cell to the end"
+  (seq
+   (conj (vec (drop 1 row)) :dead)))
 
-(defn new-cell-value [c n s e w ne nw se sw]
-  "takes the values of the currounding cells and returns the values of the new cells, values are 1 for alive 0 for dead"
-  (let [total (+ n s e w ne nw se sw)]
-    (if (= c 1)
-      (if (or (= total 2) (= total 3))
-        1
-        0)
-      (if (= total 3)
-        1
-        0))))
+(defn move-row-east [row w]
+  "moves a single row east, adding a dead cell to the start"
+  (cons :dead (take (- w 1) row)))
 
-(defn new-row-value [c n s e w]
-  "runs new-cell-value across a row. We compute the north-east, north-west ... rows by shifting rows east or west"
-  (vec
-   (map
-    #(new-cell-value %1 %2 %3 %4 %5 %6 %7 %8 %9)
-    c n s e w (move-row-east n) (move-row-west n) (move-row-east s) (move-row-west s))))
+(defn new-cell-value [current-cell-val surrounding-cells]
+  "takes the values of the surrounding cells and returns the values of the new cells, values are 1 for alive 0 for dead"
+  (let [alive-surrounding (count (filter #(= % :alive) surrounding-cells))]
+    (cond
+      (= current-cell-val :alive) (if (or (= alive-surrounding 2) (= alive-surrounding 3)) :alive :dead)
+      (= current-cell-val :dead) (if (= alive-surrounding 3) :alive :dead))
+    )
+  )
 
-(defn update-matrix [mat]
+(defn new-row-value [current n s w]
+  "runs new-cell-value across a row."
+  (map
+   #(new-cell-value %1 (list %2 %3 %4 %5 %6 %7 %8 %9))
+   current
+   n
+   s
+   (move-row-east current w)
+   (move-row-west current)
+   (move-row-east n w)
+   (move-row-west n)
+   (move-row-east s w)
+   (move-row-west s)))
+
+(defn update-matrix [matrix w h]
   "Update the matrix to the next epoch"
-  (vec (map
-   #(new-row-value %1 %2 %3 %4 %5)
-   mat
-   (get-north mat)
-   (get-south mat)
-   (get-east mat)
-   (get-west mat))))
+  (map
+   #(new-row-value %1 %2 %3 w)
+   matrix
+   (move-rows-north matrix w)
+   (move-rows-south matrix w h)))
 
-(defn render-output! [mat]
-  (doseq [l mat]
-    (println (map #(if (= % 0) " " "*") l))))
-   
+(defn render-output! [matrix]
+  (doseq [l matrix]
+    (println (map #(if (= % :dead) " " "*") l))))
 
-(defn -main
-  []
-  (loop [m (init-matrix width height)]
-         (println)
-         (render-output! m)
-         (Thread/sleep 50)
-         (recur (update-matrix m))))
-
+(defn -main [& args]
+  " first arg is width, second arg is height"
+  (do
+    (println "Enter width and height: ")
+    (loop [width (read-string (read-line)) height (read-string (read-line)) matrix (init-matrix width height)]
+      (println)
+      (render-output! matrix)
+      (Thread/sleep 100)
+      (recur width height (update-matrix matrix width height)))))
